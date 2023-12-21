25eb8fe89eafc0c2a4268cad85fae50b666e0c4c
USER <USER@DESKTOP-EATI25B>
2023-12-21 19:11:10 -0400

    Agregar archivos de proyecto.

---
+++ CapaDatos/CD_Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Conexion
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-EATI25B;DataBase=CrudForms;Integrated Security=trust");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
---
+++ CapaDatos/CD_Productos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//agragados
+
+namespace CapaDatos
+{
+    class CD_Productos
+    {
+        private CD_Conexion conexion = new CD_Conexion();
+
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "";
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+        }
+    }
+}
